26ceb1de5c9b4a72e856ce114046a39dd1745437
Alexis Gonzalez <alexisrodrigogonzalez753@gmail.com>
2022-07-05 13:34:41 -0300

    Primer ejercicio resuelto

--- unidad 4/ejercicio.js
+++ unidad 4/ejercicio.js
@@ -1,9 +1,25 @@
-var num1 = 5;
-var num2 = 5;
-var resultado = 0;
+var distancia;
 
+distancia = prompt(`Ingrese la distancia a recorrer:`, ``);
+if (distancia >= 0 && distancia <= 1000) {
+    document.write(`Su medio de transporte corresponde a Pie, ` + distancia + ` metros`);
+}
 
+else if (distancia > 1000 && distancia <= 10000) {
+    bicicleta = prompt(`Ingrese la distancia a recorrer:`, ``);
+    document.write(`Su medio de transporte corresponde en Bicicleta, ` + distancia + ` metros`);
+}
 
-resultado = num1 + num2;
+else if (distancia > 10000 && distancia <= 30000) {
+    document.write(`Su medio de transporte corresponde en Colectivo, ` + distancia + ` metros`);
+}
 
-document.write(resultado)
+else if (distancia > 30000 && distancia <= 100000) {
+    document.write(`Su medio de transporte correspode en Auto, ` + distancia + ` metros`);
+}
+
+else if(distancia >100000){
+    document.write(`Su medio de transporte corresponde en Avion, ` + distancia + ` metros`);
+}else{
+    document.write(`Error!, no se pudo calcular correctamente su distancia`)
+}
